4ac68fba24d8fcd4ef9e770f6960ca0b4f2edd80
Anna Haensch <annahaensch@gmail.com>
2021-02-22 14:37:46 -0500

    add FactoredHMMInference to __init__

--- hela/hmm/__init__.py
+++ hela/hmm/__init__.py
@@ -7,7 +7,7 @@ from .discrete_models import (DiscreteHMM, DiscreteHMMConfiguration,
                               DiscreteHMMForecasting,
                               DiscreteHMMInferenceResults,
                               DiscreteHMMValidationMetrics)
-from .factored_models import FactoredHMMConfiguration
+from .factored_models import (FactoredHMMConfiguration, FactoredHMMInference)
 from .distributed import DistributedLearningAlgorithm
 from .utils import *
 from .validation import find_risk_at_horizons
