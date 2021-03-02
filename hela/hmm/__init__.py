8d7f534cabd11308e3f43bbc961f4097565d6517
Anna Haensch <annahaensch@gmail.com>
2021-03-02 16:40:49 -0500

    update __init__

--- hela/hmm/__init__.py
+++ hela/hmm/__init__.py
@@ -7,7 +7,7 @@ from .discrete_models import (
     DiscreteHMM, DiscreteHMMConfiguration, DiscreteHMMForecasting,
     DiscreteHMMInferenceResults, DiscreteHMMValidationMetrics)
 from .factored_models import (FactoredHMMConfiguration, FactoredHMMInference,
-                              FactoredHMMLearningAlgorithm)
+                              FactoredHMMLearningAlgorithm, _factored_hmm_to_discrete_hmm)
 from .distributed import DistributedLearningAlgorithm
 from .utils import *
 from .validation import find_risk_at_horizons
