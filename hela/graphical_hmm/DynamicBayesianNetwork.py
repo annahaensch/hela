4e944000fcb396c65f35e37e1d99ce534fc90b58
Anna Haensch <annahaensch@gmail.com>
2021-03-31 08:49:14 -0400

    replace log_transition and log_initial_state

--- hela/graphical_hmm/DynamicBayesianNetwork.py
+++ hela/graphical_hmm/DynamicBayesianNetwork.py
@@ -789,11 +789,11 @@ def fhmm_model_to_graph(model):
         # latent state[t=0] -> latent state[t=1]
         graph.add_edge((current_system, 0), (current_system, 1))
         
-        transition_matrix = np.exp(model.log_transition)[i][:hidden_state,:hidden_state]
+        transition_matrix = model.transition_matrix[i][:hidden_state,:hidden_state]
         transition_cpd = TabularCPD((current_system, 1), hidden_state, transition_matrix,
                                     evidence=[(current_system, 0)], evidence_card = [hidden_state])
         
-        initial_state_vector = np.exp(model.log_initial_state)[i:i+1,:hidden_state].reshape(-1,1)
+        initial_state_vector = model.initial_state_matrix[i:i+1,:hidden_state].reshape(-1,1)
         initial_state_cpd = TabularCPD((current_system, 0), hidden_state, initial_state_vector)
         
         graph.add_factors(transition_cpd, initial_state_cpd)
